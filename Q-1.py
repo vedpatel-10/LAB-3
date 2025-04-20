082173f609c06fd29db4ea12321b4bd207a56341
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:44:12 +0530

    Update Q-1.py

--- Q-1.py
+++ Q-1.py
@@ -5,4 +5,8 @@ for i in input:
     if i == 'a' or i == 'e' or i == 'i' or i =='o' or i =='u':
         count = count + 1
 
-print("Total vowels in the given string are: " + str(count))        
+print("Total vowels in the given string are: " + str(count))     
+
+#OUTPUT:
+# Enter a string: my name is ved patel
+# Total vowels in the given string are: 6
