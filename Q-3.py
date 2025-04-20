7a45d1baebef6d35014e0b1d45955342ebc4aab2
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:44:57 +0530

    Update Q-3.py

--- Q-3.py
+++ Q-3.py
@@ -6,3 +6,8 @@ elif str2 in str1:
     print("2nd string is present in 1st string")
 else:
     print("None of the two string is present in other string")        
+
+#OUTPUT:
+# Enter first string: lion is the king of jungle
+# Enter second string: lion is the king
+# 2nd string is present in 1st string    
